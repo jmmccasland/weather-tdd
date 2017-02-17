c9f264dd735c8f36ac825df7e3d4aaf69850f823
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-17 12:13:31 -0600

    Start daily weather

--- app/get-data.js
+++ app/get-data.js
@@ -1,4 +1,4 @@
 export default function (lat, lng) {
-  return fetch(`https://api.darksky.net/forecast/${process.env.API_KEY}/${lat},${lng}`)
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/311bf310a229a347973348d2221b7956/${lat},${lng}`)
     .then(res => res.json());
 }
--- app/index.js
+++ app/index.js
@@ -1,3 +1,11 @@
 import 'whatwg-fetch';
 
 import getData from './get-data';
+import createCurrentWeatherEl from './current-weather';
+
+getData(36.1627, 86.7816)
+  .then(({ currently }) => {
+    const forecastEl = createCurrentWeatherEl(currently);
+
+    document.body.appendChild(forecastEl);
+  })
